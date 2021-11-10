432262326cf75665b4d538f5ec77cf16faf388bf
scott rosenberg <scott@terasky.com>
2021-11-10 12:11:06 +0000

    first change for text only demo#1

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + from Tanzu! With Oleg";
+		return "Greetings from Spring Boot + from Tanzu! Updated live demo #1";
 	}
 
 }
